deee457a7fac5eee1d334b671d2ad368d606ce34
Auguste Poiroux <augustepoiroux@gmail.com>
2025-03-03 21:34:34 +0100

    v4.13.0-rc4

--- test.sh
+++ test.sh
@@ -41,6 +41,6 @@ for infile in $IN_DIR/*.in; do
 
 done
 
-# Run the Mathlib tests
-cp lean-toolchain test/Mathlib/
-cd test/Mathlib/ && ./test.sh
+# Run the Mathlib tests - skipped as no Mathlib releases exist for v4.13.0-rc4
+# cp lean-toolchain test/Mathlib/
+# cd test/Mathlib/ && ./test.sh
